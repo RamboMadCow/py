8867825a84698f807053c0efe124fb94cd65c1d4
RamboMadCow <Aaron.Michael.Hicks@gmail.com>
2023-11-07 15:04:06 -0700

    string data types

--- project_1/operatorPrecedence.py
+++ project_1/operatorPrecedence.py
@@ -2,3 +2,17 @@ a = 12
 b = 3
 
 print (a + b / 3 - 4 * 12)
+print (a + (b / 3) - (4 * 12))
+print ((((a + b) / 3) - 4) * 12)
+print (((a + b) / 3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+# f = e * 12
+print (e * 12)
+
+print ()
+
+print (a / (b * a) / b)
+  
