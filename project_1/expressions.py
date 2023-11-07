9555fb93427788a905fcb48fa96819f262a6a160
RamboMadCow <Aaron.Michael.Hicks@gmail.com>
2023-11-07 14:35:10 -0700

    Expressions tutorial (#1)
    * Expressions tutorial
    
    * Committing addition of operatorPrecedence file

---
+++ project_1/expressions.py
@@ -0,0 +1,18 @@
+a = 12
+b = 3
+
+print (a + b)    # 15
+print (a - b )    # 9   
+print (a * b)    # 36
+print (a / b)    # 4.0
+print (a // b)   # 4 interger division, rounded down towards minus infinity
+print (a % b)    # 0 modulo: the remainder after integer division
+print (a ** b)   # 1728 exponentiation: a to the power of b
+
+print ()
+for i in range(1, a / b):
+    print (i)
+
+print ()
+for i in range(1, a // b):
+    print (i)
